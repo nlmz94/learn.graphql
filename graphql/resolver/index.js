1f276417f1b40f884a26f64c9c2481ffcf995724
nlmz94 <nmz94140@gmail.com>
2021-10-21 11:05:03 +0200

    time error fixed

--- graphql/resolver/index.js
+++ graphql/resolver/index.js
@@ -1,6 +1,7 @@
 import userResolver from './user.js';
 import postResolver from './post.js';
-import { GraphQLDateTime } from 'graphql-iso-date';
+import pkg from 'graphql-iso-date';
+const { GraphQLDateTime } = pkg;
 
 const customScalarResolver = { Date: GraphQLDateTime };
 export default [customScalarResolver, userResolver, postResolver];
